c42812e6a4ce8a018c883630979f620fbfacce9d
BohdanBoriak <lightraven72@gmail.com>
2024-04-09 11:00:15 +0300

    Added game MVP

--- main.go
+++ main.go
@@ -1,16 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+const (
+	totalPoints       = 10
+	pointsPerQuestion = 5
+)
 
 func main() {
-	i, j := 42, 2701
+	fmt.Println("Вітаю в моїй грі!")
+	time.Sleep(1 * time.Second)
 
-	p := &i         // вказівник на i
-	fmt.Println(*p) // читання i через вказівник
-	*p = 21         // задати i через вказівник
-	fmt.Println(i)  // вивести нове значення i
+	for i := 5; i > 0; i-- {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
 
-	p = &j         // вказівник на j
-	*p = *p / 37   // ділення j через вказівник
-	fmt.Println(j) // вивести нове значення j
+	myPoints := 0
+	start := time.Now()
+	for myPoints < totalPoints {
+		x := rand.Intn(100)
+		y := rand.Intn(100)
+
+		fmt.Printf("%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+		if err != nil {
+			fmt.Println("Спробуй ще!")
+		} else {
+			if ansInt == x+y {
+				myPoints += pointsPerQuestion
+				fmt.Printf(
+					"Чудово! Ти набрав %v балів!\nЗалишилось набрати %v\n",
+					myPoints, totalPoints-myPoints,
+				)
+			} else {
+				fmt.Println("Пощастить наступного разу!")
+			}
+		}
+	}
+	end := time.Now()
+	timeSpent := end.Sub(start)
+
+	fmt.Printf("Вітаю! Ти пройшов гру за %v!", timeSpent)
+	time.Sleep(5 * time.Second)
 }
